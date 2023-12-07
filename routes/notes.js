b069f74bd0568a039574db16f840db3158558c26
Genbuchan <contact@genbuchan.com>
2023-12-07 11:22:07 +0900

    Added yesno

--- routes/notes.js
+++ routes/notes.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const responseObjectDataAll = {
-  textObject1: {
-    id: 1,
-    title: 'ノート1のタイトルです',
-    subTitle: 'ノート1のサブタイトルです',
-    bodyText: 'ノート1の本文です'
-  },
-  textObject2: {
-    id: 2,
-    title: 'ノート2のタイトルです',
-    subTitle: 'ノート2のサブタイトルです',
-    bodyText: 'ノート2の本文です'
-  },
-};
+const { MongoClient } = require("mongodb");
+const uri = "";
+const client = new MongoClient(uri);
 
 /**
  * メモを全件取得するAPI
@@ -23,9 +12,15 @@ const responseObjectDataAll = {
  * @returns {string} data.title - タイトル
  * @returns {string} data.text - 内容
  */
-router.get('/', function(req, res, next) {
+router.get('/', async (req, res) => {
   // 全件取得する
-  res.json(responseObjectDataAll);
+  const database = client.db('notes');
+  const notes = database.collection('notes');
+
+  const query = { id: 1 };
+  const note = await notes.findOne(query);
+
+  res.json(note);
 });
 
 module.exports = router;
